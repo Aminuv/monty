5e2373d0fb5b74b97d77b007487181cd9b12c618
khizzioui97 <khiziouii@gmail.com>
2023-06-22 18:54:38 -0700

    my

---
+++ rotl.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+
+/**
+ * rotl - push the data in the stack or queue
+ * @stack: pointer to the stack
+ * @line_number: instruction
+ */
+void _rotl(stack_t **stack, unsigned int line_number)
+{
+	if (!(*stack) || !(*stack)->next)
+	{
+		return;
+	}
+	else
+	{
+		stack_t *tmp = *stack;
+		*stack = (*stack)->next;
+		(*stack)->prev = NULL;
+		add_dnod_eint_end(stack, tmp->n);
+		free(tmp);
+	}
+	(void)line_number;
+}
